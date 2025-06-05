223d22862e43675a3335b221ddf233758e1f7799
Luchezar31 <luchezar31@abv.bg>
2025-06-06 00:27:37 +0300

    adding database

--- workshop/settings.py
+++ workshop/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'workshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,9 +73,13 @@ WSGI_APPLICATION = 'workshop.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "workshop",
+        "USER": "postgres",
+        "PASSWORD": "ivaylo2009",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
